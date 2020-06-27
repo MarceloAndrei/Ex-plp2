82f39556ba76e63219ca89eef3cd181baed5942f
Dan <danivasco@msn.com>
2020-06-27 18:08:26 -0300

    Revert "da uma olhada ae mandrei"
    This reverts commit 66fa77ae9d0024f9a4445f405123f758ace3a6fa.

--- medicamento.py
+++ medicamento.py
@@ -1,6 +1,5 @@
 class Medicamento:
-    def __init__(self, nome):
+    def __init__(self,nome):
         self.__nome = nome
-
     def nome_medicamento(self):
         return self.__nome
--- paciente.py
+++ paciente.py
@@ -1,22 +1,15 @@
 from pessoa import Pessoa
-
-
 class Paciente(Pessoa):
     i = 0
-
     def __init__(self):
         i = i+1
-
     def __del__(self):
         i = i-1
-
     def definir_id(self, identificacao):
         if len(identificacao) > 5:
             print("Erro")
         else:
             self._identificacao = identificacao
-
-    def definir_prontuario(self, Prontuario):
-
+    def definir_prontuário(self,Prontuario):
     def pacientes_ativos(self):
         return i
